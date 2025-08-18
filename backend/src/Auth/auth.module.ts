f33614f25ada74d1a1802a705fc89ec6c73cb524
Abdelrhman <abdelrhman.ayman9876@gmail.com>
2025-08-19 00:12:38 +0300

    update auth.module.ts

--- backend/src/Auth/auth.module.ts
+++ backend/src/Auth/auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PrismaModule } from 'src/DataBase/prisma.module';
+import { GithubStrategy } from './strategy/gethub.strategy';
 
 @Module({
   imports: [PrismaModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, GithubStrategy],
 })
 export class AuthModule {}

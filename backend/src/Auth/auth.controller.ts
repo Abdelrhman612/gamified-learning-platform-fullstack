7ea632c8e5169c396aeeae50c1b246933a5fc12e
Abdelrhman <abdelrhman.ayman9876@gmail.com>
2025-08-19 00:12:28 +0300

    update auth.controller.ts

--- backend/src/Auth/auth.controller.ts
+++ backend/src/Auth/auth.controller.ts
@@ -1,9 +1,10 @@
 import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { MeResponseDto, SignInDto, SignUpDto } from './dto/auth.create.dto';
-import { AuthGuard } from './auth.guard';
-import { JwtPayload } from './types/auth.types';
+import { AuthGuards } from './auth.guard';
+import { JwtPayload, OAuthUser } from './types/auth.types';
 import { User } from 'src/utils/decorators/user.decorator';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -18,12 +19,23 @@ export class AuthController {
     return this.authService.signIn(signInDto);
   }
   @Get('me')
-  @UseGuards(AuthGuard)
+  @UseGuards(AuthGuards)
   getMe(@User() user: JwtPayload): MeResponseDto {
     return {
       name: user.name,
       email: user.email,
       role: user.role,
     };
+  }
+  @Get('github/sign-in')
+  @UseGuards(AuthGuard('github'))
+  githubSignIn() {
+    return { message: 'Redirecting to GitHub for authentication' };
+  }
+  @Get('github/callback')
+  @UseGuards(AuthGuard('github'))
+  async githubCallback(@User() profile: OAuthUser) {
+    const user = await this.authService.validateOAuthUser(profile);
+    return { user };
   }
 }

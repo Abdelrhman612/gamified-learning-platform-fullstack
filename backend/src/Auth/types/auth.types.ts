e52886524e94efef32c26ee50e0a78e70ee432ad
Abdelrhman <abdelrhman.ayman9876@gmail.com>
2025-08-19 00:13:00 +0300

    update auth.types.ts

--- backend/src/Auth/types/auth.types.ts
+++ backend/src/Auth/types/auth.types.ts
@@ -4,3 +4,9 @@ export interface JwtPayload {
   email: string;
   role: string;
 }
+export interface OAuthUser {
+  id: string;
+  username: string;
+  email: string | null;
+  accessToken: string;
+}

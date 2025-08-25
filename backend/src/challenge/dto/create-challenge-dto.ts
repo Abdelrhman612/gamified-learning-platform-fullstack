4a28d317ee26d06a063bf4dc676a7f6ae02bb161
Abdelrhman <abdelrhman.ayman9876@gmail.com>
2025-08-25 04:37:49 +0300

    update create-challenge-dto.ts

--- backend/src/challenge/dto/create-challenge-dto.ts
+++ backend/src/challenge/dto/create-challenge-dto.ts
@@ -3,14 +3,19 @@ import { IsDate, IsNumber, IsString } from 'class-validator';
 export class CreateChallengeDto {
   @IsString()
   title: string;
+
   @IsString()
   description: string;
+
   @IsString()
   type: string;
+
   @IsNumber()
   points: number;
+
   @IsDate()
   startAt: Date;
+
   @IsDate()
   endAt: Date;
 }

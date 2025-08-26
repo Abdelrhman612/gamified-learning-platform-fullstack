3f727b6fef70199262c27abf81ae50e7ddc1495d
Abdelrhman <abdelrhman.ayman9876@gmail.com>
2025-08-26 17:38:21 +0300

    update gemini.controller.ts

--- backend/src/gemini/gemini.controller.ts
+++ backend/src/gemini/gemini.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { GeminiService } from './gemini.service';
 import { GetAiMessageDto } from './dto/get-ai-response.dto';
+import { AuthGuards } from 'src/Auth/auth.guard';
 
 @Controller('gemini')
+@UseGuards(AuthGuards)
 export class GeminiController {
   constructor(private readonly geminiService: GeminiService) {}
   @Post()

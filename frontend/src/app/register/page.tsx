5644ce49bfa98cb68ed268902bcaddd6b97831c3
Abdelrhman <abdelrhman.ayman9876@gmail.com>
2025-09-01 09:06:12 +0300

    update page.tsx

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { SignUp } from "../lib/endpoints/challenge";
+import { SignUp } from "../lib/endpoints/auth";
 
 export default function RegisterPage() {
   const [name, setName] = useState("");

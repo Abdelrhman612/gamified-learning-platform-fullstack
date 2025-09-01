8e3c95830d89b4cf9e8ddaa921f485bb7c7c088f
Abdelrhman <abdelrhman.ayman9876@gmail.com>
2025-09-01 07:53:22 +0300

    feat: create user.ts

---
+++ frontend/src/app/lib/endpoints/user.ts
@@ -0,0 +1,25 @@
+import { User } from "@/app/dashboard/interface.user";
+import axios from "axios";
+import { userUrl } from "../api";
+
+export const getUsers = async (): Promise<User[]> => {
+  const token = localStorage.getItem("token");
+  const res = await axios.get(userUrl, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return res.data;
+};
+
+export const updateUser = async (id: string, data: User) => {
+  const res = await axios.patch(`${userUrl}/${id}`, data, {
+    headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+  });
+  return res.data;
+};
+
+export const deleteUser = async (id: string) => {
+  const res = await axios.delete(`${userUrl}/${id}`, {
+    headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+  });
+  return res.data;
+};

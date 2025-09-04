68c296ce8159c6e0dbb45b40c487176a88582463
Abdelrhman <abdelrhman.ayman9876@gmail.com>
2025-09-04 17:56:11 +0300

    update api.ts

--- frontend/src/app/lib/api.ts
+++ frontend/src/app/lib/api.ts
@@ -4,4 +4,4 @@ export const getHubCallbackurl = `${process.env.NEXT_PUBLIC_API_URL}/auth/github
 export const userUrl = `${process.env.NEXT_PUBLIC_API_URL}/user`;
 export const meUrl = `${process.env.NEXT_PUBLIC_API_URL}/auth/me`;
 export const challengesUrl = `${process.env.NEXT_PUBLIC_API_URL}/challenges`;
-export const participationsUrl = `${process.env.NEXT_PUBLIC_API_URL}/challenges`;
+export const participationsUrl = `${process.env.NEXT_PUBLIC_API_URL}/challenges-participations`;

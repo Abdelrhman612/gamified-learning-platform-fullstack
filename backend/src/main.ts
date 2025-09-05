bbf199092731241f39287215c00eb96a7b7d3f9a
Abdelrhman <abdelrhman.ayman9876@gmail.com>
2025-09-05 18:54:20 +0300

    update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,20 +4,31 @@ import { RequestMethod, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const allowedOrigins = [
+    process.env.CORS_ORIGIN,
+    'http://localhost:3000',
+    'https://gamified-learning-platform-fullstack-4s6u-nna83t521.vercel.app',
+    'https://gamified-learning-platform-fullstack-4s6u-pavwfcs5o.vercel.app',
+  ];
+
   app.enableCors({
-    origin: process.env.CORS_ORIGIN,
+    origin: allowedOrigins,
     credentials: true,
   });
+
   app.setGlobalPrefix('api/v1', {
     exclude: [{ path: '/', method: RequestMethod.GET }],
   });
 
-  await app.listen(process.env.PORT || 3000);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
+
+  await app.listen(process.env.PORT || 3000);
 }
+
 void bootstrap();

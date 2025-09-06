6cf6782b8047a1fa985a9499b63dbf68c815386b
Abdelrhman <abdelrhman.ayman9876@gmail.com>
2025-09-06 14:15:08 +0300

    update 1 file and create 1 file

--- frontend/src/app/lib/api.ts
+++ frontend/src/app/lib/api.ts
@@ -1,8 +1,10 @@
-export const signInUrl = `${process.env.NEXT_PUBLIC_API_URL}/auth/sign-in`;
-export const signUpUrl = `${process.env.NEXT_PUBLIC_API_URL}/auth/sign-up`;
-export const getHubCallbackurl = `${process.env.NEXT_PUBLIC_API_URL}/auth/github/callback`;
-export const userUrl = `${process.env.NEXT_PUBLIC_API_URL}/user`;
-export const meUrl = `${process.env.NEXT_PUBLIC_API_URL}/auth/me`;
-export const challengesUrl = `${process.env.NEXT_PUBLIC_API_URL}/challenges`;
-export const participationsUrl = `${process.env.NEXT_PUBLIC_API_URL}/challenges-participations`;
-export const geminiUrl = `${process.env.NEXT_PUBLIC_API_URL}/gemini`;
+import { config } from "@/config";
+
+export const signInUrl = `${config.apiUrl}/auth/sign-in`;
+export const signUpUrl = `${config.apiUrl}/auth/sign-up`;
+export const getHubCallbackurl = `${config.apiUrl}/auth/github/callback`;
+export const userUrl = `${config.apiUrl}/user`;
+export const meUrl = `${config.apiUrl}/auth/me`;
+export const challengesUrl = `${config.apiUrl}/challenges`;
+export const participationsUrl = `${config.apiUrl}/challenges-participations`;
+export const geminiUrl = `${config.apiUrl}/gemini`;
